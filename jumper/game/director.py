0d2816c02b06d52f69dc13737fd40c04b669ee8c
ZuluDelta117 <97050214+ZuluDelta117@users.noreply.github.com>
2022-10-18 17:49:13 -0600

    Add files via upload

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -32,7 +32,7 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
-        new_location = self._terminal_service.read_number("\nEnter a location [1-1000]: ")
+        new_location = self._terminal_service.read_word("\nGuess a letter [a-z]: ")
         self._seeker.move_location(new_location)
         
     def _do_updates(self):
@@ -53,4 +53,3 @@ class Director:
         self._terminal_service.write_text(hint)
         if self._hider.is_found():
             self._is_playing = False
-        pass
--- jumper/game/terminal_service.py
+++ jumper/game/terminal_service.py
@@ -1,5 +1,10 @@
 class TerminalService:
-    #Obviously we want to set this class up to display to the terminal - Jordan
+    # Obviously we want to set this class up to display to the terminal - Jordan
     def read_text(self, prompt):
         return input(prompt)
-        pass
+    
+    def read_word(self, prompt):
+        return str(input(prompt))
+
+    def write_word(self, word):
+        print(word)
